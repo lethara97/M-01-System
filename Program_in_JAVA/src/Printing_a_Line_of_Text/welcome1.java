22ccbd3a10abb0c9320626fc1e9cfcfe55c28fe7
Keshara1997 <kavindakeshara97@gmail.com>
2021-11-17 16:02:09 +0530

    modify

---
+++ Program_in_JAVA/src/Printing_a_Line_of_Text/welcome1.java
@@ -0,0 +1,12 @@
+package Printing_a_Line_of_Text;
+
+public class welcome1 {
+	// main method begins execution of Java application
+	public static void main(String[] args)
+	{
+	System.out.print("Welcome to ");
+	System.out.println("Java Programming!");
+	} // end method main
+} // end class Welcome2
+
+
