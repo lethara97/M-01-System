77b1bc1043e5b204fc66fc7e36e95271b6f16082
Keshara1997 <kavindakeshara97@gmail.com>
2021-11-17 21:15:39 +0530

    Printing line a text

---
+++ JAVA_Program/src/PrintTextLine/welcome.java
@@ -0,0 +1,9 @@
+package PrintTextLine;
+
+public class welcome {
+	// main method begins execution of Java application
+	public static void main(String[] args)
+	{
+	System.out.println("Welcome to Java Programming!");
+	} // end method main
+} // end class Welcome1
