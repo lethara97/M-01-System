64e9e7bd140a206f747353384d3420455a6055a2
Keshara1997 <kavindakeshara97@gmail.com>
2021-11-18 07:24:44 +0530

    i used print and println tag

---
+++ JAVA_Program/src/PrintTextLine/welcome1.java
@@ -0,0 +1,12 @@
+package PrintTextLine;
+
+public class welcome1 {
+	// main method begins execution of Java application
+	public static void main(String[] args) 
+	{
+		System.out.print("Welcome to + ");
+		System.out.println("JAVA PROGRAM!!!");
+		
+	}
+
+}// end class welcome1
