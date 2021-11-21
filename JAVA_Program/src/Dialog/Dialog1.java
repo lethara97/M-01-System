1587ffbf3aac4247f7d2608de635a17385d30e2e
Keshara1997 <kavindakeshara97@gmail.com>
2021-11-22 04:50:22 +0530

    show message dialog

---
+++ JAVA_Program/src/Dialog/Dialog1.java
@@ -0,0 +1,13 @@
+package Dialog;
+import javax.swing.JOptionPane;
+
+
+public class Dialog1 {
+	public static void main(String[] args)
+	{
+	// display a dialog with a message
+	JOptionPane.showMessageDialog(null, "Welcome to Java");
+	}
+	
+
+}
