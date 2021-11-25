981308dce3e8d5465e0a3f98862775fb84e9ba5e
Keshara1997 <kavindakeshara97@gmail.com>
2021-11-25 07:29:48 +0530

    Counter-controlled repetition with the for repetition statement.

---
+++ JAVA_Program/src/Operators/ForCounter.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class ForCounter {
+	public static void main(String[] args) {
+		for(int counter = 0; counter<10; counter++){
+			System.out.printf("%d ", counter);
+		}
+	}
+
+}
