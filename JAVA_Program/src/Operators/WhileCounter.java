f250d74547238a650cfd3b83459867f882ca157f
Keshara1997 <kavindakeshara97@gmail.com>
2021-11-25 07:24:35 +0530

    Counter-controlled repetition with the while repetition statement.

---
+++ JAVA_Program/src/Operators/WhileCounter.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class WhileCounter {
+
+	public static void main(String[] args) {
+		int counter =1;
+		
+		while (counter <= 10) {
+			System.out.printf("%d %n",counter);
+			++counter;
+		}
+		
+	}
+}
--- JAVA_Program/src/Panel/DrawPanel.java
+++ JAVA_Program/src/Panel/DrawPanel.java
@@ -1,21 +1,18 @@
 package Panel;
-//import java.awt.Graphics;
-//import java.swing.JPanel;
-
 
 import java.awt.Graphics;
 import javax.swing.JPanel;
 public class DrawPanel extends JPanel
 {
-// draws an X from the corners of the panel
+
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		int width = getWidth(); // total width
-		int height = getHeight(); // total height
-			// draw a line from the upper-left to the lower-right
+		int width = getWidth(); 
+		int height = getHeight(); 
+
 			g.drawLine(0, 0, width, height);
-			// draw a line from the lower-left to the upper-right
+		
 			g.drawLine(0, height, width, 0);
 	}
 }
