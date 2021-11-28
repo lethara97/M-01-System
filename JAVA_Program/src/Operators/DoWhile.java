eed6449af0591d15f587e9ef5d9cd66273f565e5
Keshara1997 <kavindakeshara97@gmail.com>
2021-11-29 01:31:00 +0530

    do...while repetition statement.

---
+++ JAVA_Program/src/Operators/DoWhile.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class DoWhile {
+	public static void main(String[] args)
+	{
+	int counter = 1;
+	do
+	{
+	System.out.printf("%d ", counter);
+	++counter;
+	} while (counter <= 10); // end do...while
+	System.out.println();
+	}
+
+}
