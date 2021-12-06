6e80f0d14c9924f8cdb149e9db3d99635fcc5d58
Kavinda Keshara <63952119+Keshara1997@users.noreply.github.com>
2021-12-06 21:41:03 +0530

    Calculate the volume of a sphere (#37)
    * Scope class demonstrates field and local variable scopes
    
    * calculate the volume of a sphere

---
+++ JAVA_Program/src/Methods/Sphere.java
@@ -0,0 +1,20 @@
+package Methods;
+import java.util.Scanner;
+public class Sphere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter radius of sphere: ");
+		double radius = input.nextDouble();
+		System.out.printf("Volume is %f%n", sphereVolume(radius));
+		} // end method determineSphereVolume
+		// calculate and return sphere volume
+		public static double sphereVolume(double radius)
+		{
+		double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+		return volume;
+		}
+
+	}
+
