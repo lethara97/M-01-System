a1aeabd22c7877daa9694adad7943d06b499b33d
Kavinda Keshara <63952119+Keshara1997@users.noreply.github.com>
2021-12-08 12:00:42 +0530

    Initializing the elements of an array to default values of zero (#39)
    * Scope class demonstrates field and local variable scopes
    
    * calculate the volume of a sphere
    
    * initializing the elements of an array to default values of zero

--- JAVA_Program/src/Methods/Sphere.java
+++ JAVA_Program/src/Methods/Sphere.java
@@ -5,11 +5,19 @@ public class Sphere {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
+ test
+		
+		System.out.print("Enter radius of sphere: ");
+		
+		double radius = input.nextDouble();
+		
+		System.out.printf("Volume is %f%n", sphereVolume(radius));
+		} 
+	
 		System.out.print("Enter radius of sphere: ");
 		double radius = input.nextDouble();
 		System.out.printf("Volume is %f%n", sphereVolume(radius));
-		} // end method determineSphereVolume
-		// calculate and return sphere volume
+main
 		public static double sphereVolume(double radius)
 		{
 		double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
