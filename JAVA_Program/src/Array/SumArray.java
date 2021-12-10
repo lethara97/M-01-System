5ebc4db8041a9ef40eb99842bd50a0c5e650cf95
Kavinda Keshara <63952119+Keshara1997@users.noreply.github.com>
2021-12-10 11:33:57 +0530

    Calculating the values to be placed into the elements of an array (#40)
    * Scope class demonstrates field and local variable scopes
    
    * calculate the volume of a sphere
    
    * initializing the elements of an array to default values of zero
    
    * Initializing the elements of an array with an array initializer
    
    * initializing the elements of an array with an array initializer
    
    * Calculating the values to be placed into the elements of an array
    
    * Computing the sum of the elements of an array
    
    Co-authored-by: Keshara Bandara <88639083+bandara1997@users.noreply.github.com>

---
+++ JAVA_Program/src/Array/SumArray.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class SumArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array= {87,9,4,8,8,7,5,8,6,89,57,62,64,64,25};
+		int total =0;
+		
+		for(int counter =0; counter<array.length; counter++) {
+			total += array[counter];
+			
+		}
+		System.out.printf("total of array elements : %d%n ",total);
+	}
+
+}
