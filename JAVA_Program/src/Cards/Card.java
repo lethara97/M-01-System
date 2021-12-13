84ff0a3ed4e39d62df5658fc131ea26adf6c1a73
Keshara1997 <kavindakeshara97@gmail.com>
2021-12-13 14:08:12 +0530

    Card Shuffling and Dealing Simulation

---
+++ JAVA_Program/src/Cards/Card.java
@@ -0,0 +1,18 @@
+package Cards;
+
+public class Card {
+
+	private final String face;
+	private final String suit;
+	
+	public Card(String cardFace, String cardSuit)
+	{
+	this.face = cardFace; 
+	this.suit = cardSuit; 
+	}
+
+	public String toString()
+	{
+	return face + " of " + suit;
+	}
+}
---
+++ JAVA_Program/src/Cards/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author keshara
+ *
+ */
+package Cards;
