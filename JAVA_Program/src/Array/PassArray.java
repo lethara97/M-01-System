3a4266f5165b6eec75821c76db284db03cbc4bbd
Keshara1997 <kavindakeshara97@gmail.com>
2021-12-22 21:42:15 +0530

    Passing arrays and individual array elements to methods.

--- JAVA_Program/src/Array/PassArray.java
+++ JAVA_Program/src/Array/PassArray.java
@@ -5,6 +5,33 @@ public class PassArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}//fvsm'bgbggbgmbk'gnbkgnkgnblkgb
-
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.printf("Effects of passing reference to entire array:%n" +
+		"The values of the original array are:%n");
+		
+		for (int value : array)
+		System.out.printf("%d", value);
+		modifyArray(array);
+		System.out.printf("%n%nThe values of the modified array are:%n");
+		
+		for (int value : array)
+		System.out.printf("%d", value);
+		System.out.printf(
+		"%n%nEffects of passing array element value:%n" +
+		"array[3] before modifyElement: %d%n", array[3]);
+		modifyElement(array[3]); 
+		System.out.printf(
+		"array[3] after modifyElement: %d%n", array[3]);
+	}
+	public static void modifyArray(int[] array2)
+	{
+	for (int counter = 0; counter < array2.length; counter++)
+	array2[counter] *= 2;
+	}
+	public static void modifyElement(int element)
+	{
+	element *= 2;
+	System.out.printf(
+	"Value of element in modifyElement: %d%n", element);
+	}
 }
