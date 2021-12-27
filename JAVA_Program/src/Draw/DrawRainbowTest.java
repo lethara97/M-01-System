1ec7fefa39bb9d6be2e3af51283100cc0dc0fe39
Keshara1997 <kavindakeshara97@gmail.com>
2021-12-27 20:23:46 +0530

    application to display rainbow

---
+++ JAVA_Program/src/Draw/DrawRainbowTest.java
@@ -0,0 +1,18 @@
+package Draw;
+
+import javax.swing.JFrame;
+
+public class DrawRainbowTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DrawRainbow panel = new DrawRainbow();
+		JFrame application = new JFrame();
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.add(panel);
+		application.setSize(400, 250);
+		application.setVisible(true);
+		
+	}
+
+}
