fdcd83daae2842e405eb8f7ad795c6e1eb3334b8
Keshara1997 <kavindakeshara97@gmail.com>
2021-12-29 20:02:12 +0530

    edit

--- JAVA_Program/src/Array/new1.java
+++ JAVA_Program/src/Array/new1.java
@@ -4,12 +4,7 @@ public class new1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//000000000
-		///
-		
-		
-		//2655
-		//errdffrrrrrrrrrrrrrrrrgggggggggggggggggdddddddddd
+
 	}
 
 }
