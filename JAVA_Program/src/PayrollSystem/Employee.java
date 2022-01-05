fee49c9780e9aaf182e3c0518bd35de3d654c5d6
Keshara1997 <kavindakeshara97@gmail.com>
2022-01-05 19:05:55 +0530

    Abstract classes

---
+++ JAVA_Program/src/PayrollSystem/Employee.java
@@ -0,0 +1,10 @@
+package PayrollSystem;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JAVA_Program/src/PayrollSystem/SalariedEmployee.java
@@ -0,0 +1,5 @@
+package PayrollSystem;
+
+public class SalariedEmployee {
+
+}
