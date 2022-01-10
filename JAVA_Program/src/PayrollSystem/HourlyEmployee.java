9ea75d09187dee507f6a278dd1e7a110670c2261
Keshara1997 <kavindakeshara97@gmail.com>
2022-01-10 07:18:20 +0530

    base plus commission employee class complete

--- JAVA_Program/src/PayrollSystem/HourlyEmployee.java
+++ JAVA_Program/src/PayrollSystem/HourlyEmployee.java
@@ -30,6 +30,10 @@ public class HourlyEmployee {
 	 * @param wage the wage to set
 	 */
 	public void setWage(double wage) {
+		if (wage < 0.0) {
+			throw new IllegalArgumentException(
+			"Hourly wage must be >= 0.0");
+		}
 		this.wage = wage;
 		
 	}
@@ -45,8 +49,28 @@ public class HourlyEmployee {
 	 * @param hours the hours to set
 	 */
 	public void setHours(double hours) {
+		if ((hours < 0.0) || (hours > 168.0)) { // validate hours
+			throw new IllegalArgumentException(
+			"Hours worked must be >= 0.0 and <= 168.0");
+		}
 		this.hours = hours;
 	}
+
+	public double earnings()
+	{
+	if (getHours() <= 40) // no overtime
+	return getWage() * getHours();
+	else
+	return 40 * getWage() + (getHours() - 40) * getWage() * 1.5;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("hourly employee: %s%n%s: $%,.2f; %s: %,.2f",
+				super.toString(), "hourly wage", getWage(),
+				"hours worked", getHours());
+		}
+	
 	
 
 }
