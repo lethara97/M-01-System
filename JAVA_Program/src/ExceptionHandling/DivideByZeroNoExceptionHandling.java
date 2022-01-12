b47c99e429f3499398ae12b819e1f4c23dc1354a
Keshara1997 <kavindakeshara97@gmail.com>
2022-01-13 04:58:44 +0530

    integer division without exception handling

--- JAVA_Program/src/ExceptionHandling/DivideByZeroNoExceptionHandling.java
+++ JAVA_Program/src/ExceptionHandling/DivideByZeroNoExceptionHandling.java
@@ -1,10 +1,24 @@
 package ExceptionHandling;
 
+import java.util.Scanner;
 public class DivideByZeroNoExceptionHandling {
 	
+	public static int qutient(int numerator , int denaminator) {
+		return numerator / denaminator;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Emter an integer numerator : ");
+		int numerator= scanner.nextInt();
+		
+		System.out.print("Enter an integer denaminator : ");
+		int denaminator = scanner.nextInt();
+		
+		int result = qutient(numerator,denaminator);
+		
+		System.out.printf("%nResult : %d / %d = %d%n",numerator,denaminator,result);
 
 	}
 
