a0815498cbcfecf0b80b8de1b74c848012faebba
Keshara1997 <kavindakeshara97@gmail.com>
2022-01-23 20:52:50 +0530

    display text and images in a window

--- JAVA_Program/src/JLabel/LabelTest.java
+++ JAVA_Program/src/JLabel/LabelTest.java
@@ -1,10 +1,14 @@
 package JLabel;
-
+import javax.swing.JFrame;
 public class LabelTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		LabelFrame labelFrame = new LabelFrame();
+		labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		labelFrame.setSize(260, 180);
+		labelFrame.setVisible(true);
 	}
 
 }
