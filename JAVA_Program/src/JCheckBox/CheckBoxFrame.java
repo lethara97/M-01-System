093ac4d2c8b21d1c52c1bbce7ac03b45ffee312e
Keshara1997 <kavindakeshara97@gmail.com>
2022-01-26 21:52:29 +0530

    jCheckBox classes

---
+++ JAVA_Program/src/JCheckBox/CheckBoxFrame.java
@@ -0,0 +1,5 @@
+package JCheckBox;
+
+public class CheckBoxFrame {
+
+}
---
+++ JAVA_Program/src/JCheckBox/CheckBoxTest.java
@@ -0,0 +1,10 @@
+package JCheckBox;
+
+public class CheckBoxTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
