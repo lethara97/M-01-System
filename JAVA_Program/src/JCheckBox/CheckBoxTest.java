098312c509a1cf3c5504f71c44b64fd6cc07c77a
Keshara1997 <kavindakeshara97@gmail.com>
2022-01-27 19:58:46 +0530

    uses two JCheckBoxes to select the desired font style of the text displayed in a JTextField.

--- JAVA_Program/src/JCheckBox/CheckBoxTest.java
+++ JAVA_Program/src/JCheckBox/CheckBoxTest.java
@@ -1,10 +1,16 @@
 package JCheckBox;
 
+import javax.swing.JFrame;
+
 public class CheckBoxTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		CheckBoxFrame checkBoxFrame = new CheckBoxFrame();
+		checkBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		checkBoxFrame.setSize(275,100);
+		checkBoxFrame.setVisible(true);
+		
 	}
 
 }
