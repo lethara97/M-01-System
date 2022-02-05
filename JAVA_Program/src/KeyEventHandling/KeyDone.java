7cf875bea6bcdfc3aadde4bd7b9fa99e2de62387
Keshara1997 <kavindakeshara97@gmail.com>
2022-02-05 21:19:05 +0530

    key event hanndling

---
+++ JAVA_Program/src/KeyEventHandling/KeyDone.java
@@ -0,0 +1,16 @@
+package KeyEventHandling;
+
+import javax.swing.JFrame;
+
+public class KeyDone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		KeyDemoFrame keyDemoFrame = new KeyDemoFrame();
+		keyDemoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		keyDemoFrame.setSize(350, 100);
+		keyDemoFrame.setVisible(true);
+	}
+
+}
