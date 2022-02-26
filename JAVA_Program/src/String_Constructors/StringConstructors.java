46b3af3f06754061207b49adaa8ea841ed7f9993
Kavinda Keshara <63952119+Keshara1997@users.noreply.github.com>
2022-02-27 00:21:21 +0530

    String class constructors. (#109)
    * rotate around origin and draw stars in random colors
    
    * String class constructors

---
+++ JAVA_Program/src/String_Constructors/StringConstructors.java
@@ -0,0 +1,22 @@
+package String_Constructors;
+
+public class StringConstructors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char[] charArray = { 'b','i','r','t','h','_','d','a','y'};
+		String s= new String("hello");
+	
+		String s1 = new String();
+		String s2 = new String(s);
+		String s3 = new String(charArray);
+		String s4 = new String(charArray,6,3);
+		
+		
+		System.out.printf(
+				"s1= %s%ns2 =%s%ns3 = %s%ns4 =%s%n",s1,s2,s3,s4);
+		
+	}
+
+}
