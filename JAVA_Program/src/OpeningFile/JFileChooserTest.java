f3b15230a5880f5fbac5d065f915d318f6f92948
User <kavindakeshara97@gmail.com>
2022-05-30 09:04:52 +0530

    Demonstrating JFileChooser 	and Tests class JFileChooserDemo.

--- JAVA_Program/src/OpeningFile/JFileChooserTest.java
+++ JAVA_Program/src/OpeningFile/JFileChooserTest.java
@@ -1,10 +1,18 @@
 package OpeningFile;
 
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
 public class JFileChooserTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  throws IOException{
 		// TODO Auto-generated method stub
 
+		JFileChooserDemo application = new JFileChooserDemo();
+		application.setSize(400, 400); 
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.setVisible(true); 
 	}
 
 }
