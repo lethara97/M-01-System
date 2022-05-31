58be13112ad7d17c01014646d07141f6d591936d
Kavinda Keshara <63952119+Keshara1997@users.noreply.github.com>
2022-05-31 14:27:54 +0530

    Test (#134)
    * create file
    
    * Demonstrating JFileChooser 	and Tests class JFileChooserDemo.
    
    Co-authored-by: Keshara Bandara <88639083+bandara1997@users.noreply.github.com>

--- JAVA_Program/src/OpeningFile/JFileChooserTest.java
+++ JAVA_Program/src/OpeningFile/JFileChooserTest.java
@@ -1,10 +1,26 @@
 package OpeningFile;
 
+ test
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public class JFileChooserTest {
+
+	public static void main(String[] args)  throws IOException{
+		// TODO Auto-generated method stub
+
+		JFileChooserDemo application = new JFileChooserDemo();
+		application.setSize(400, 400); 
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.setVisible(true); 
+
 public class JFileChooserTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+ main
 	}
 
 }
