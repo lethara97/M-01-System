093d8f31b1042c9bbf395cce18edf2f054bc3400
User <kavindakeshara97@gmail.com>
2022-06-19 20:13:10 +0530

    Demonstrating lambdas and streams with an array of Integers.

---
+++ JAVA_Program/src/StreamIntegerManipulations/ArraysAndStreams.java
@@ -0,0 +1,49 @@
+package StreamIntegerManipulations;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.List;
+
+public class ArraysAndStreams {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer[] values = {2, 9, 5, 0, 3, 7, 1, 4, 8, 6};
+		
+		 // display original values
+		 System.out.printf("Original values: %s%n", Arrays.asList(values));
+		
+		 
+		// sort values in ascending order with streams
+		  System.out.printf("Sorted values: %s%n", 
+		  Arrays.stream(values) 
+		 .sorted() 
+		 .collect(Collectors.toList()));
+		 
+		  // values greater than 4
+		 List<Integer> greaterThan4 = 
+		 Arrays.stream(values) 
+		 		.filter(value -> value > 4) 
+		 		.collect(Collectors.toList());
+		 System.out.printf("Values greater than 4: %s%n", greaterThan4);
+		 
+		  // filter values greater than 4 then sort the results
+		 System.out.printf("Sorted values greater than 4: %s%n",
+				 Arrays.stream(values) 
+				 .filter(value -> value > 4) 
+				 .sorted() 
+				 .collect(Collectors.toList()) );
+		 
+		 // greaterThan4 List sorted with streams
+		  System.out.printf(
+		  "Values greater than 4 (ascending with streams): %s%n",
+		  greaterThan4.stream() 
+		  .sorted() 
+		  .collect(Collectors.toList())
+		  
+		  );
+	}
+
+}
