7bbdce49e4f94b44fe9bdc23cf176450b02fe110
Keshara1997 <kavindakeshara97@gmail.com>
2022-07-05 22:37:59 +0530

    Towers of Hanoi solution with a recursive method.

---
+++ JAVA_Program/src/TowersofHanoi/TowersOfHanoi.java
@@ -0,0 +1,36 @@
+package TowersofHanoi;
+
+public class TowersOfHanoi {
+	
+	
+	public static void solveTowers(int disks, int sourcePeg,
+			int destinationPeg, int tempPeg)
+			{
+			// base case -- only one disk to move
+			if (disks == 1)
+			{
+			System.out.printf("%n%d --> %d", sourcePeg, destinationPeg);
+			return;
+			}
+			// recursion step -- move (disk - 1) disks from sourcePeg
+			// to tempPeg using destinationPeg
+			solveTowers(disks - 1, sourcePeg, tempPeg, destinationPeg);
+			// move last disk from sourcePeg to destinationPeg
+			System.out.printf("%n%d --> %d", sourcePeg, destinationPeg);
+			// move (disks - 1) disks from tempPeg to destinationPeg
+			solveTowers(disks - 1, tempPeg, destinationPeg, sourcePeg);
+			}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int startPeg = 1; // value 1 used to indicate startPeg in output
+		int endPeg = 3; // value 3 used to indicate endPeg in output
+		int tempPeg = 2; // value 2 used to indicate tempPeg in output
+		int totalDisks = 3; // number of disks
+		
+		solveTowers(totalDisks, startPeg, endPeg, tempPeg);
+
+	}
+
+}
